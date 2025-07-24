c538ed80e19a342cef2ec4e1a7b80608a5e5e0b4
boribloblack <sungminyang020704@gmail.com>
2025-07-25 02:34:03 +0900

    Question 3 Answer

--- week8/streamlit_app3.py
+++ week8/streamlit_app3.py
@@ -38,8 +38,7 @@ def plot_advanced_sunspot_visualizations(df, sunactivity_col='SUNACTIVITY',
         xs = np.linspace(data.min(), data.max(), 200)
         density = gaussian_kde(data)
 
-        '''코드를 작성하시오'''
-
+        axs[0, 1].hist(data, bins=30, density=True, alpha=0.6, color='gray', label='Histogram')
         axs[0, 1].plot(xs, density(xs), color='red', linewidth=2, label='Density')
     axs[0, 1].set_title("Distribution of Sunspot Activity")
     axs[0, 1].set_xlabel("Sunspot Count")
